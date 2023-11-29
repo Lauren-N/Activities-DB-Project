07fefd67da8c6ee137f20e661940a24c40d9bb98
Lauren Nguyen <lnguyen9@ada.gonzaga.edu>
2023-11-28 20:17:04 -0800

    odd SQL errors for foreign keys

--- project_tables.sql
+++ project_tables.sql
@@ -6,13 +6,13 @@
  **********************************************************************/
 
 -- TODO: add drop table statements
-DROP TABLE IF EXISTS Student;
-DROP TABLE IF EXISTS Courses_in;
-DROP TABLE IF EXISTS Courses;
-DROP TABLE IF EXISTS Clubs_in;
-DROP TABLE IF EXISTS Clubs;
-DROP TABLE IF EXISTS Activities;
 DROP TABLE IF EXISTS Activities_in;
+DROP TABLE IF EXISTS Activities;
+DROP TABLE IF EXISTS Clubs;
+DROP TABLE IF EXISTS Clubs_in;
+DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Courses_in;
+DROP TABLE IF EXISTS Student;
 
 -- TODO: add create table statements
 CREATE TABLE Student(
@@ -32,14 +32,15 @@ CREATE TABLE Courses_in(
     year_completed YEAR,
     grade_given CHAR(2),
     long_desc CHAR(250),
-    PRIMARY KEY(student_id, class_id)
+    PRIMARY KEY(student_id, class_id),
+    FOREIGN KEY(class_id) REFERENCES Courses(course_class_id)
 );
 
 CREATE TABLE Courses(
-    class_id CHAR(100),
-    class_name CHAR(100),
-    long_desc CHAR(250),
-    PRIMARY KEY(class_id)
+    course_class_id CHAR(100),
+    course_class_name CHAR(100),
+    course_long_desc CHAR(250),
+    PRIMARY KEY(course_class_id)
 );
 
 CREATE TABLE Clubs_in(
@@ -85,6 +86,28 @@ INSERT INTO Student VALUES ('mshrull', 'Madeleine', 'Shrull', 20, 'Gonzaga', 'Ps
 INSERT INTO Student VALUES ('mmikesell', 'Marlee', 'Mikesell', 21, 'Gonzaga', 'Political Science');
 INSERT INTO Student VALUES ('etollett', 'Ellie', 'Tollett', 20, 'Gonzaga', 'Psychology');
 
+-- inserting values for courses in DB
+INSERT INTO Courses VALUES ('CPSC321', 'Database Management Systems', 'Introduction to relational database concepts and techniques.');
+INSERT INTO Courses VALUES ('CPSC333', 'Mobile App Development','This course provides an introduction to mobile application development.');
+INSERT INTO Courses VALUES ('CPSC323', 'Machine Learning and Intelligent Systems', 'A look into ML...');
+INSERT INTO Courses VALUES ('CPSC346', 'Operating Systems', 'Study of operating systems internals.');
+INSERT INTO Courses VALUES ('MATH321', 'Statistics for Experimentalists', 'An applied statistics course for those with calculus preparation.');
+INSERT INTO Courses VALUES ('LIT105', 'Literature of the Open Road', 'This course introduces students to literary study through the exploration of a particular theme.');
+
+INSERT INTO Courses VALUES ('BIO105', 'Info Flow in Biol Systems', 'This course provides a foundation in the principles of biology by examining the fundamental role of information in generating the properties of life.');
+INSERT INTO Courses VALUES ('CPSC223', 'Algorithm and Abstract Data Structures', 'Algorithm analysis using Big-O notation, sorting, heaps, balanced binary search trees, and hash tables.');
+INSERT INTO Courses VALUES ('CPSC222', 'Into to Data Science', 'This course provides an introduction to the underlying ideas, concepts, and techniques used in data science.');
+INSERT INTO Courses VALUES ('HIST201', 'History of the US', 'This course surveys North American history from the continents first peopling through the end of the U.S. Civil War.');
+INSERT INTO Courses VALUES ('MATH231', 'Discrete Structures', 'A study of propositional logic, set theory, functions, algorithms, divisibility, introductory number theory, elementary proof techniques, counting techniques, recursive definitions, mathematical induction, and graph theory.');
+
+INSERT INTO Courses VALUES ('CPSC121', 'Computer Science 1', 'An introduction to programming. Emphasis is on how to design, code, debug, and document programs using good programming style.');
+INSERT INTO Courses VALUES ('MATH157', 'Calculus 1', 'An introduction to calculus for engineering, science and mathematics students, with an emphasis on conceptual understanding, problem solving, and modeling.');
+INSERT INTO Courses VALUES ('EDPE157', 'Yoga', 'This class is designed to promote individual fitness for total mind/body health.');
+INSERT INTO Courses VALUES ('ENVS193', 'Digital Ecology', 'The First-Year Seminar (FYS) introduces new Gonzaga students to the University, the Core Curriculum, and Gonzagas Jesuit mission and heritage.');
+INSERT INTO Courses VALUES ('ENGL101', 'Writing', 'This course helps students develop the foundational skills of critical reading, thinking, analysis, and writing.');
+INSERT INTO Courses VALUES ('THEA100', 'Introduction to Theatre Arts', 'An introductory survey of the history, aesthetics, and literature of the theatre, and the various areas of theatrical production.');
+
+
 -- inserting values for courses a student is in
 INSERT INTO Courses_in VALUES ('lnguyen9', 'CPSC321', 'Database Management Systems', 2023, 'A', 'Introduction to relational database concepts and techniques.');
 INSERT INTO Courses_in VALUES ('lnguyen9', 'CPSC333', 'Mobile App Development', 2023, 'B', 'This course provides an introduction to mobile application development.');
@@ -105,27 +128,6 @@ INSERT INTO Courses_in VALUES ('lnguyen9', 'EDPE157', 'Yoga', 2021, 'A', 'This c
 INSERT INTO Courses_in VALUES ('lnguyen9', 'ENVS193', 'Digital Ecology', 2021, 'A', 'The First-Year Seminar (FYS) introduces new Gonzaga students to the University, the Core Curriculum, and Gonzagas Jesuit mission and heritage.');
 INSERT INTO Courses_in VALUES ('lnguyen9', 'ENGL101', 'Writing', 2021, 'B', 'This course helps students develop the foundational skills of critical reading, thinking, analysis, and writing.');
 INSERT INTO Courses_in VALUES ('lnguyen9', 'THEA100', 'Introduction to Theatre Arts', 2021, 'A', 'An introductory survey of the history, aesthetics, and literature of the theatre, and the various areas of theatrical production.');
-
--- inserting values for courses in DB
-INSERT INTO Courses VALUES ('CPSC321', 'Database Management Systems', 'Introduction to relational database concepts and techniques.');
-INSERT INTO Courses VALUES ('CPSC333', 'Mobile App Development','This course provides an introduction to mobile application development.');
-INSERT INTO Courses VALUES ('CPSC323', 'Machine Learning and Intelligent Systems', 'A look into ML...');
-INSERT INTO Courses VALUES ( 'CPSC346', 'Operating Systems', 'Study of operating systems internals.');
-INSERT INTO Courses VALUES ('MATH321', 'Statistics for Experimentalists', 'An applied statistics course for those with calculus preparation.');
-INSERT INTO Courses VALUES ('LIT105', 'Literature of the Open Road', 'This course introduces students to literary study through the exploration of a particular theme.');
-
-INSERT INTO Courses VALUES ('BIO105', 'Info Flow in Biol Systems', 'This course provides a foundation in the principles of biology by examining the fundamental role of information in generating the properties of life.');
-INSERT INTO Courses VALUES ('CPSC223', 'Algorithm and Abstract Data Structures', 'Algorithm analysis using Big-O notation, sorting, heaps, balanced binary search trees, and hash tables.');
-INSERT INTO Courses VALUES ('CPSC222', 'Into to Data Science', 'This course provides an introduction to the underlying ideas, concepts, and techniques used in data science.');
-INSERT INTO Courses VALUES ('HIST201', 'History of the US', 'This course surveys North American history from the continents first peopling through the end of the U.S. Civil War.');
-INSERT INTO Courses VALUES ('MATH231', 'Discrete Structures', 'A study of propositional logic, set theory, functions, algorithms, divisibility, introductory number theory, elementary proof techniques, counting techniques, recursive definitions, mathematical induction, and graph theory.');
-
-INSERT INTO Courses VALUES ('CPSC121', 'Computer Science 1', 'An introduction to programming. Emphasis is on how to design, code, debug, and document programs using good programming style.');
-INSERT INTO Courses VALUES ('MATH157', 'Calculus 1', 'An introduction to calculus for engineering, science and mathematics students, with an emphasis on conceptual understanding, problem solving, and modeling.');
-INSERT INTO Courses VALUES ('EDPE157', 'Yoga', 'This class is designed to promote individual fitness for total mind/body health.');
-INSERT INTO Courses VALUES ('ENVS193', 'Digital Ecology', 'The First-Year Seminar (FYS) introduces new Gonzaga students to the University, the Core Curriculum, and Gonzagas Jesuit mission and heritage.');
-INSERT INTO Courses VALUES ('ENGL101', 'Writing', 'This course helps students develop the foundational skills of critical reading, thinking, analysis, and writing.');
-INSERT INTO Courses VALUES ('THEA100', 'Introduction to Theatre Arts', 'An introductory survey of the history, aesthetics, and literature of the theatre, and the various areas of theatrical production.');
 
 
 -- inserting clubs in
